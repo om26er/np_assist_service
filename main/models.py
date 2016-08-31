b7b954f45ef6f329abb14361761716f1db4301ae
Omer Akram <om26er@ubuntu.com>
2016-08-31 10:45:27 +0500

    Age is not a Char

--- main/models.py
+++ main/models.py
@@ -24,7 +24,7 @@ class Property(models.Model):
     postcode = models.CharField(max_length=255, blank=False)
     category_primary = models.CharField(max_length=255, blank=False)
     category_secondary = models.CharField(max_length=255, blank=False)
-    age = models.IntegerField(blank=False)
+    age = models.CharField(max_length=255, blank=False)
 
     def __str__(self):
         return 'Property of {}'.format(self.owner.email)
